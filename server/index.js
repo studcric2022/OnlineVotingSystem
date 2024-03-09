e03be7a8443bf8da6702fe7c88252f7a4cf98d1c
studcric2022 <rohitkumarsingh2021@gmail.com>
2024-03-09 21:46:21 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+
+const handle =require('./handlers');
+const cors = require('cors');
+const port=process.env.PORT;
+const bodyParser=require('body-parser');
+const app= express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>
+   res.json({hello:'worlds'})
+);
+app.use(handle.notFound);
+app.use(handle.errors);
+
+app.listen(port,console.log('Server is running on 5000'));
